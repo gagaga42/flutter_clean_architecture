03ed378714b361f60925696c6ffe1fa381c20af3
minjun-P <jun7332568@gmail.com>
2023-10-08 00:34:07 +0900

    feat : 회원가입 뷰 완성

--- lib/app/presentation/screens/sign_up_screen/sign_up_screen.dart
+++ lib/app/presentation/screens/sign_up_screen/sign_up_screen.dart
@@ -9,10 +9,43 @@ class SignUpScreen extends GetView<SignUpScreenController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('sign_up'))
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("회원가입"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: controller.idCon,
+              decoration: const InputDecoration(labelText: "아이디"),
+            ),
+            TextFormField(
+              controller: controller.nameCon,
+              decoration: const InputDecoration(labelText: "이름"),
+            ),
+            TextFormField(
+              controller: controller.pwdCon,
+              decoration: const InputDecoration(labelText: "비밀번호"),
+            ),
+            TextFormField(
+              controller: controller.pwdCheckCon,
+              decoration: const InputDecoration(labelText: "비밀번호 확인"),
+            ),
+            const SizedBox(height: 20),
+            GetBuilder<SignUpScreenController>(
+              builder: (controller) {
+                return ElevatedButton(
+                  onPressed: controller.allowSubmit?controller.onSubmit : null,
+                  child: const Text("제출"),
+                );
+              }
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
